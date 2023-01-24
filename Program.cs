8b0fd2b23dcc093a268ca819c94a73b0160c9c8e
Ramazan <coldalbard.@gmail.com>
2023-01-25 01:26:02 +0300

    Added a solution for point 4.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿// Console.WriteLine("Enter the length of the array: ");
-// int size = Convert.ToInt32(Console.ReadLine());
-
-// string[] CreateArray(int size){
+﻿// string[] CreateArray(int size){
 //     string[] array = new string [size];
 //     Console.WriteLine("Enter the array elements: ");
 //     for(int i = 0; i < size; i++){
@@ -9,15 +6,53 @@
 //     }
 //     return array;
 // }
-
-// string[] arr = CreateArray(size);
-
+// // Функция для создания массива с длиной которую задает пользователь
 
 // void PrintArray(string[] arr){
+//     Console.Write("[");
 //     for(int i = 0; i < arr.Length; i++){
-//         Console.Write($"{arr[i]} ");
+//         Console.Write($"{arr[i]}, ");
 //     }
-//     Console.WriteLine();
+//     Console.Write("]");
 // }
+// // Функция для вывода массива пользователя на экран
+
+// string[] Symbol(string[] arr) {   
+//     int count = 0;
+//     int index = 0;
+//     for (int i = 0; i < arr.Length; i++) { 
+//         if (arr[i].Length < 4) 
+//         {
+//             count++;
+//         }
+//     }
+//     // Нашел длину для нового массива, записал в переменную count
+//     string[] res = new string[count];
+//     for (int j = 0; j < arr.Length; j++) { 
+//         if (arr[j].Length < 4) 
+//         {
+//             res[index] = arr[j];
+//             index++;
+//         }
+//     }
+//     // Заполнил массив res, через условие
+//     return res;
+// }
+// // Функция для формирования нового массива, который будет удовлетворять требование:
+// // из имеющегося массива строк формирует массив из строк, длина которых меньше либо 
+// // равна 3 символа
+
+
+// Console.WriteLine("Please enter the length of your array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// string[] arr = CreateArray(size);
+// // Сохраняю в переменную arr, результат выполнения функции CreateArray
 
 // PrintArray(arr);
+
+// string[] result = Symbol(arr);
+// // Сохраняю в переменную result, результат выполнения функции Symbol
+
+// Console.Write("->");
+// PrintArray(result);
