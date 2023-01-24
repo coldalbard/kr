736c5ca17917f9f6d491f3d5a4dd60e00d86d805
Ramazan <coldalbard.@gmail.com>
2023-01-24 22:39:02 +0300

    added new file + gitignore

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter the length of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] CreateArray(int size){
+    string[] array = new string [size];
+    Console.Write("Enter the array elements: ");
+    for(int i = 0; i < size; i++){
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+string[] arr = CreateArray(size);
