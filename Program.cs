ee6cf722691383a5bb5c5bb87c7758bbffb2c4dd
Ramazan <coldalbard.@gmail.com>
2023-01-24 22:42:58 +0300

    added PrintArray

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
-﻿Console.WriteLine("Enter the length of the array: ");
-int size = Convert.ToInt32(Console.ReadLine());
+﻿// Console.WriteLine("Enter the length of the array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
 
-string[] CreateArray(int size){
-    string[] array = new string [size];
-    Console.Write("Enter the array elements: ");
-    for(int i = 0; i < size; i++){
-        array[i] = Console.ReadLine();
-    }
-    return array;
-}
+// string[] CreateArray(int size){
+//     string[] array = new string [size];
+//     Console.WriteLine("Enter the array elements: ");
+//     for(int i = 0; i < size; i++){
+//         array[i] = Console.ReadLine();
+//     }
+//     return array;
+// }
 
-string[] arr = CreateArray(size);
+// string[] arr = CreateArray(size);
+
+
+// void PrintArray(string[] arr){
+//     for(int i = 0; i < arr.Length; i++){
+//         Console.Write($"{arr[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// PrintArray(arr);
